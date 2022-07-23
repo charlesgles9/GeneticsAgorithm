6a0db1d1ee9b1166b2d3f92b4c22faf20533e4a3
charlie <charlesgles9@gmail.com>
2022-07-23 09:59:29 +0300

    genetics algorithm

---
+++ src/com/genetics/app/Chromosome.java
@@ -0,0 +1,11 @@
+package com.genetics.app;
+
+public class Chromosome {
+
+	private String strand="";
+	private char []array;
+	public Chromosome(String strand) {
+		this.strand=strand;
+		
+	}
+}
---
+++ src/com/genetics/app/Main.java
@@ -0,0 +1,9 @@
+package com.genetics.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+	}
+
+}
